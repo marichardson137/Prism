8ec268413ca81f36677f5d211350f08a126a40d0
maricha6 <maricha6@ncsu.edu>
2024-06-11 08:23:52 -0400

    [Cube] very close to face rendering

--- src/main.c
+++ src/main.c
@@ -7,11 +7,16 @@
 typedef struct {
     int numVertices;
     Vector3* vertices;
+    int* indices;
 } Face;
 
 void DrawFace(Face face, Color color)
 {
-    DrawTriangleStrip3D(face.vertices, face.numVertices, color);
+    Vector3 vertices[face.numVertices];
+    for (int i = 0; i < face.numVertices; i++) {
+        vertices[i] = face.vertices[face.indices[i]];
+    }
+    DrawTriangleStrip3D(vertices, face.numVertices, color);
 }
 
 void DrawText3D(Camera camera, Vector3 position, const char* text, int fontSize, Color color)
@@ -39,6 +44,23 @@ int main(void)
     // Mesh mesh = GenMeshSphere(3, 6, 9);
     Mesh mesh = GenMeshCube(2, 2, 2);
 
+    int vl[6][4] = {
+        { 0, 1, 3, 2 },
+        { 4, 0, 3, 5 },
+        { 7, 4, 5, 6 },
+        { 1, 7, 6, 2 },
+        { 5, 3, 2, 6 },
+        { 4, 7, 1, 0 }
+    };
+
+    Face faces[6];
+
+    for (int i = 0; i < 6; i++) {
+        faces[i].numVertices = 4;
+        faces[i].vertices = (Vector3*)(mesh.vertices);
+        faces[i].indices = vl[i];
+    }
+
     // Update loop
     while (!WindowShouldClose()) {
 
@@ -57,6 +79,10 @@ int main(void)
         for (int i = 0; i < mesh.vertexCount; i++) {
             Vector3* pos = (Vector3*)(mesh.vertices + 3 * i);
             DrawSphere(*pos, 0.1f, RED);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            DrawFace(faces[i], WHITE);
         }
 
         EndMode3D();
