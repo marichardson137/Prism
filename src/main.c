d48afe822480632bf4b0cddcdb1f140b302bcbc2
maricha6 <maricha6@ncsu.edu>
2024-06-13 11:33:15 -0400

    [Extrusion] moving faces vertically and by normal

--- src/main.c
+++ src/main.c
@@ -47,23 +47,13 @@ void _TriangulateAndDrawPolygon(_Model model, _Polygon polygon, Camera camera)
     Vector3 normal = Vector3CrossProduct(edge2, edge1);
     normal = Vector3Normalize(normal);
 
-    Vector3 center = Vector3Lerp(v1, v3, 0.5);
-    DrawSphere(center, 0.1f, RED);
-    Ray ray = { center, normal };
-    DrawRay(ray, GREEN);
-
-    Vector3 up = { 0.0f, 1.0f, 0.0f };
-    Ray ray2 = { center, up };
-    DrawRay(ray2, BLUE);
-
     // Create a rotation matrix to align the normal with the z-axis
+    Vector3 up = { 0.0f, 1.0f, 0.0f };
     Matrix rotationMatrix;
     if (Vector3Equals(up, Vector3Negate(normal))) {
         rotationMatrix = MatrixScale(-1, 1, 1);
     } else {
         Vector3 rotationAxis = Vector3CrossProduct(normal, up);
-        Ray ray3 = { center, rotationAxis };
-        DrawRay(ray3, RED);
         float angle = acosf(Vector3DotProduct(normal, up));
         rotationMatrix = MatrixRotate(rotationAxis, angle);
     }
@@ -73,7 +63,6 @@ void _TriangulateAndDrawPolygon(_Model model, _Polygon polygon, Camera camera)
     // Project each vertex onto the 2D plane
     for (int i = 0; i < polygon.numIndices; i++) {
         _Vertex v = model.vertices[polygon.indices[i]];
-        DrawSphere(v, 0.2f, BLUE);
         // Rotate the vertex to align the polygon's plane with the z-axis
         Vector3 rotatedVertex = Vector3Transform(v, rotationMatrix);
         // Drop the z-coordinate
@@ -153,6 +142,8 @@ int main(void)
     camera.up = (Vector3) { 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
 
+    int selectedPolygon = 0;
+
     // Update loop
     while (!WindowShouldClose()) {
 
@@ -171,17 +162,60 @@ int main(void)
 
         DrawGrid(10, 1.0f);
 
+        if (IsKeyPressed(KEY_ZERO)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 0;
+            model.polygons[selectedPolygon].color = GREEN;
+        } else if (IsKeyPressed(KEY_ONE)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 1;
+            model.polygons[selectedPolygon].color = GREEN;
+        } else if (IsKeyPressed(KEY_TWO)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 2;
+            model.polygons[selectedPolygon].color = GREEN;
+        } else if (IsKeyPressed(KEY_THREE)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 3;
+            model.polygons[selectedPolygon].color = GREEN;
+        } else if (IsKeyPressed(KEY_FOUR)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 4;
+            model.polygons[selectedPolygon].color = GREEN;
+        } else if (IsKeyPressed(KEY_FIVE)) {
+            model.polygons[selectedPolygon].color = BEIGE;
+            selectedPolygon = 5;
+            model.polygons[selectedPolygon].color = GREEN;
+        }
+
         if (IsKeyDown(KEY_W)) {
-            model.vertices[6].y += 0.02f;
-            model.vertices[7].y += 0.02f;
-            model.vertices[3].y += 0.02f;
-            model.vertices[2].y += 0.02f;
+            _Polygon p = model.polygons[selectedPolygon];
+            for (int i = 0; i < p.numIndices; i++) {
+                model.vertices[p.indices[i]].y += 0.05f;
+            }
         }
         if (IsKeyDown(KEY_S)) {
-            model.vertices[6].y -= 0.02f;
-            model.vertices[7].y -= 0.02f;
-            model.vertices[3].y -= 0.02f;
-            model.vertices[2].y -= 0.02f;
+            _Polygon p = model.polygons[selectedPolygon];
+            for (int i = 0; i < p.numIndices; i++) {
+                model.vertices[p.indices[i]].y -= 0.05f;
+            }
+        }
+        if (IsKeyDown(KEY_N)) {
+            _Polygon p = model.polygons[selectedPolygon];
+            _Vertex v1 = model.vertices[p.indices[0]];
+            _Vertex v2 = model.vertices[p.indices[1]];
+            _Vertex v3 = model.vertices[p.indices[2]];
+
+            Vector3 edge1 = Vector3Subtract(v2, v1);
+            Vector3 edge2 = Vector3Subtract(v3, v1);
+            Vector3 normal = Vector3CrossProduct(edge2, edge1);
+            normal = Vector3Normalize(normal);
+            normal = Vector3Scale(normal, 0.05f);
+            for (int i = 0; i < p.numIndices; i++) {
+                model.vertices[p.indices[i]].x += normal.x;
+                model.vertices[p.indices[i]].y += normal.y;
+                model.vertices[p.indices[i]].z += normal.z;
+            }
         }
 
         for (int i = 0; i < 6; i++) {
@@ -194,6 +228,8 @@ int main(void)
             char text[2];
             sprintf(text, "%d", i);
             DrawText3D(camera, vertices[i], text, 20, RAYWHITE);
+            Vector2 screenPos = GetWorldToScreen(vertices[i], camera);
+            DrawCircle(screenPos.x, screenPos.y, 5, BLUE);
         }
 
         EndDrawing();
